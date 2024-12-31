60a437d4b4a14b3d62f8340f73dafff54e428c5a
Bruno Cardamone <brunocardamone03@gmail.com>
2024-12-31 16:39:19 -0300

    fix: filter fix and add validations

--- src/service/tools/filter.js
+++ src/service/tools/filter.js
@@ -1,5 +1,9 @@
 export const filterTasks = (filter, array) => {
-  const filterTasks = array.filter(
+  if (array.message) return [];
+
+  if (array.length === 0) return array;
+
+  const filterTasks = array?.filter(
     (task) => task?.Estado?.toLocaleLowerCase() === filter.toLocaleLowerCase()
   );
   return filterTasks;
